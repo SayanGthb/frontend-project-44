0eaa838de2e8da3268de7d4c64d9eff0a4d47554
Sayan <smiiithh@yandex.ru>
2024-02-12 20:12:05 +0300

    fix: update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -11,6 +11,8 @@ const isPrimeNumber = () => {
   const question = randomNumber;
 
   const isPrime = (num) => {
+    if (num === 1) return false;
+
     for (let i = 2; i <= Math.sqrt(num); i += 1) {
       if (num % i === 0) {
         return false;
