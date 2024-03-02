68fe857af6ea52aa0be66c357d5795b4000fda6c
Sayan <smiiithh@yandex.ru>
2024-03-02 17:36:06 +0300

    fix: change name function game to getRound and delete const max_random_array

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,13 +4,12 @@ const description = 'What is the result of the expression?';
 
 const MIN_RANDOM = 1;
 const MAX_RANDOM = 50;
-const MAX_RANDOM_ARRAY = 3;
 const signArray = ['+', '-', '*'];
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
 let answer;
 
-const game = () => {
-  const randomIndexSignArray = getRandomNumber(MIN_RANDOM, MAX_RANDOM_ARRAY);
+const getRound = () => {
+  const randomIndexSignArray = getRandomNumber(MIN_RANDOM, signArray.length);
   const randomNumber = getRandomNumber(MIN_RANDOM, MAX_RANDOM);
   const randomNumber2 = getRandomNumber(MIN_RANDOM, MAX_RANDOM);
 
@@ -31,4 +30,4 @@ const game = () => {
   return [question, String(answer)];
 };
 
-export default () => app(description, game);
+export default () => app(description, getRound);
