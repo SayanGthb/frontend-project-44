b19baf8b672320c604977c1a5831f846ed479d39
Sayan <smiiithh@yandex.ru>
2024-03-02 18:56:53 +0300

    fix: update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,12 +6,13 @@ const description = 'Find the greatest common divisor of given numbers.';
 const MIN_RANDOM = 1;
 const MAX_RANDOM = 100;
 
+const getGreatestCommonDivisor = (a, b) => (b === 0 ? a : getGreatestCommonDivisor(b, (a % b)));
+
 const game = () => {
   const randomNumber = getRandomNumber(MIN_RANDOM, MAX_RANDOM);
   const randomNumber2 = getRandomNumber(MIN_RANDOM, MAX_RANDOM);
   const question = `${randomNumber} ${randomNumber2}`;
-  const gcd = (a, b) => (b === 0 ? a : gcd(b, (a % b)));
-  const answer = gcd(randomNumber, randomNumber2);
+  const answer = getGreatestCommonDivisor(randomNumber, randomNumber2);
   return [question, String(answer)];
 };
 
