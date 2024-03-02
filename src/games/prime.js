d2319f56a92ec2e4524e063aaa60d4d53a5ea2a3
Sayan <smiiithh@yandex.ru>
2024-03-02 19:02:29 +0300

    fix: update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,25 +5,24 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const MIN_RANDOM = 1;
 const MAX_RANDOM = 100;
+const answerArray = ['yes', 'no'];
 
-const isPrimeNumber = () => {
+const isPrime = (num) => {
+  if (num === 1) return false;
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getAnswer = () => {
   const randomNumber = getRandomNumber(MIN_RANDOM, MAX_RANDOM);
   const question = randomNumber;
-
-  const isPrime = (num) => {
-    if (num === 1) return false;
-
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  const answer = isPrime(randomNumber) ? 'yes' : 'no';
-
+  const answer = isPrime(randomNumber) ? answerArray[0] : answerArray[1];
   return [question, answer];
 };
 
-export default () => app(description, isPrimeNumber);
+export default () => app(description, getAnswer);
