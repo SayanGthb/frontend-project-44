ba214ae039d3336ee7b7f51368f1587fb091eaa8
Sayan <smiiithh@yandex.ru>
2024-03-16 23:21:16 +0300

    fix: remove answerArray from even.js

--- src/games/even.js
+++ src/games/even.js
@@ -5,13 +5,12 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 const MIN_RANDOM = 1;
 const MAX_RANDOM = 100;
-const answerArray = ['yes', 'no'];
 
 const isEven = (number) => number % 2 === 0;
 
 const getAnswer = () => {
   const question = getRandomNumber(MIN_RANDOM, MAX_RANDOM);
-  const answer = isEven(question) ? answerArray[0] : answerArray[1];
+  const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
